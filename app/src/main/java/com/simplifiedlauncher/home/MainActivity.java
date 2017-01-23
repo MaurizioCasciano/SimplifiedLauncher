4663628292a039da79165aa1be29fb055ae4a34b
izio7 <mauriziocasciano7@gmail.com>
2017-01-23 15:43:05 +0100

    Permessi runtime

--- app/src/main/java/com/simplifiedlauncher/home/MainActivity.java
+++ app/src/main/java/com/simplifiedlauncher/home/MainActivity.java
@@ -1,17 +1,22 @@
 package com.simplifiedlauncher.home;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.example.maurizio.simplifiedlauncher.R;
+import com.simplifiedlauncher.permissions.RuntimePermissionsActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends RuntimePermissionsActivity {
+    public static final String TAG = MainActivity.class.getName();
     Button telefono;
     Button rubrica;
     Button sms;
@@ -20,15 +25,34 @@ public class MainActivity extends AppCompatActivity {
     Button galleria;
     Intent i;
     LinearLayout container;
+    public static String[] permissions = new String[]{
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.WRITE_CONTACTS,
+            Manifest.permission.READ_SMS,
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.RECEIVE_SMS,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        container=(LinearLayout) findViewById(R.id.container);
+        if (!super.checkPermission(permissions)) {
+            Log.d(TAG, "onCreate: permissions granted");
+        } else {
+            Log.d(TAG, "onCreate: permissions denied");
+            super.requestAppPermissions(permissions, R.id.activity_main, 1236);
+        }
 
-        telefono= (Button) findViewById(R.id.telefono);
+
+        container = (LinearLayout) findViewById(R.id.container);
+
+        telefono = (Button) findViewById(R.id.telefono);
         rubrica = (Button) findViewById(R.id.rubrica);
         sms = (Button) findViewById(R.id.sms);
         chiamate_rapide = (Button) findViewById(R.id.chiamate_rapide);
@@ -38,13 +62,13 @@ public class MainActivity extends AppCompatActivity {
         telefono.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getActionMasked()){
+                switch (event.getActionMasked()) {
                     case MotionEvent.ACTION_DOWN:
                         telefono.setBackgroundColor(Color.YELLOW);
                         break;
                     case MotionEvent.ACTION_UP:
                         telefono.setBackgroundColor(Color.WHITE);
-                        i= new Intent(getApplicationContext(),Telefono.class);
+                        i = new Intent(getApplicationContext(), Telefono.class);
                         startActivity(i);
                         break;
                 }
@@ -55,13 +79,13 @@ public class MainActivity extends AppCompatActivity {
         sms.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getActionMasked()){
+                switch (event.getActionMasked()) {
                     case MotionEvent.ACTION_DOWN:
                         sms.setBackgroundColor(Color.YELLOW);
                         break;
                     case MotionEvent.ACTION_UP:
                         sms.setBackgroundColor(Color.WHITE);
-                        i= new Intent(getApplicationContext(),GestioneMessaggi.class);
+                        i = new Intent(getApplicationContext(), GestioneMessaggi.class);
                         startActivity(i);
                         break;
                 }
@@ -72,13 +96,13 @@ public class MainActivity extends AppCompatActivity {
         rubrica.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getActionMasked()){
+                switch (event.getActionMasked()) {
                     case MotionEvent.ACTION_DOWN:
                         rubrica.setBackgroundColor(Color.YELLOW);
                         break;
                     case MotionEvent.ACTION_UP:
                         rubrica.setBackgroundColor(Color.WHITE);
-                        i= new Intent(getApplicationContext(),Rubrica.class);
+                        i = new Intent(getApplicationContext(), Rubrica.class);
                         startActivity(i);
                         break;
                 }
@@ -89,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
         chiamate_rapide.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getActionMasked()){
+                switch (event.getActionMasked()) {
                     case MotionEvent.ACTION_DOWN:
                         chiamate_rapide.setBackgroundColor(Color.YELLOW);
                         break;
@@ -104,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
         fotocamera.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getActionMasked()){
+                switch (event.getActionMasked()) {
                     case MotionEvent.ACTION_DOWN:
                         fotocamera.setBackgroundColor(Color.YELLOW);
                         break;
@@ -118,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
         galleria.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getActionMasked()){
+                switch (event.getActionMasked()) {
                     case MotionEvent.ACTION_DOWN:
                         galleria.setBackgroundColor(Color.YELLOW);
                         break;
@@ -129,6 +153,11 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode) {
+
     }
 
 
